9c1e0ba629ece13a022c0b2a60dc2626dbdf0dff
Sayyed Ameen Naqvi <sayyedameennaqvi0430@gmail.com>
2025-11-03 19:49:35 +0530

    Remove commented-out class component code

--- src/components/App.js
+++ src/components/App.js
@@ -30,3 +30,44 @@ function App(){
       </div>
     )
 }
+
+// class App extends React.Component{
+
+//   constructor(props){
+//     super(props)
+//     this.state = {
+//       markdown: "",
+//       loading: true
+//     }
+//     this.handleChange = this.handleChange.bind(this);
+//     this.changeLoadingVar = this.changeLoadingVar.bind(this);
+//   }
+//   handleChange(e){
+//     this.setState({
+//       markdown: e.target.value
+//     })
+//   }
+//   changeLoadingVar(){
+//     this.setState({
+//       loading: false
+//     })
+//   }
+
+//   componentDidMount(){
+//     setTimeout(this.changeLoadingVar,3000)
+//   }
+//   render(){
+//     if(this.state.loading){
+//       return (<div><h1>Loading...</h1></div>)
+//     }
+ 
+//     return(
+//       <div className="app">
+//         <textarea onChange={this.handleChange}/>
+//         <ReactMarkdown className="preview" children={this.state.markdown}/>
+//       </div>
+//     )
+//   }
+// }
+
+export default App;
